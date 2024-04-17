7cb9bb1e4358d773ac2b94b7fefe0ab61020fe32
hurrsy1982 <1279915496@qq.com>
2024-04-17 16:50:17 +0800

    继续完善MyBatis Generator 自定义插件 BaseEnHanceCommentGenerator,作用是增强DefaultCommentGenerator中一些功能. 完善ji个Plugin功能

--- lnutcm-backend-sanzhu-mybatis-generator-plugins/src/main/java/com/sanzhu/mybatis/generator/plugins/target/ExampleTargetPlugin.java
+++ lnutcm-backend-sanzhu-mybatis-generator-plugins/src/main/java/com/sanzhu/mybatis/generator/plugins/target/ExampleTargetPlugin.java
@@ -15,7 +15,7 @@ import static org.mybatis.generator.internal.util.messages.Messages.getString;
  * 作者：胡蓉蓉<br>
  * 模块：Mybatis Generator 逆向工程<br>
  * 描述：1.编写目的:自定义项目中逆向工程的Example类的生成位置<br>
- *      2.实现需求:生成的Example类不和model类放置在一个文件夹<br>
+ * 2.实现需求:生成的Example类不和model类放置在一个文件夹<br>
  * <p>
  * 备注：禁止商用<br>
  * ------------------------------------------------------------<br>
@@ -26,45 +26,50 @@ import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
 public class ExampleTargetPlugin extends PluginAdapter {
 
-    private String  targetPackage;
+    private String targetPackage;
 
     /**
      * Function name:initialized<br>
-     * Params:<br>
-     * param  warnings List<String> (向列表中添加字符串以指定警告。例如，如果<br>
-     *        插件无效，您应该指定原因。警告为在运行完成后报告给用户)<br>
-     * return:boolean
      * Inside the function:<br>
-     * 方法在对内省表调用getGeneratedXXXFiles方法之前调用的。插件
-     * 可以实现此方法来覆盖任何默认属性，或更改数据库的结果内省，在任何代码生成活动发生之前。<br>
+     *                 方法在对内省表调用getGeneratedXXXFiles方法之前调用的。<br>
+     *                 插件可以实现此方法来覆盖任何默认属性，或更改数据库的结果内省，在任何代码生成活动发生之前。<br>
+     * return:boolean<br>
+     * Params:<br>
+     * @param warnings List<String> (向列表中添加字符串以指定警告。例如，如果<br>
+     *                 插件无效，您应该指定原因。警告为在运行完成后报告给用户)<br>
+
      */
     @Override
     public boolean validate(List<String> warnings) {
         this.targetPackage = properties.getProperty("targetPackage"); //Example生成的目标文件夹
         boolean valid = stringHasValue(targetPackage);//判断配置的字符串是否为空
-        if (valid){
+        if (valid) {
             warnings.add(getString("ValidationError.18", //$NON-NLS-1$
                     "ExampleTargetPlugin", //$NON-NLS-1$
                     "searchString")); //$NON-NLS-1$
         }
         return valid;
     }
+
     /**
      * Function name:initialized<br>
-     * Params:<br>
-     * param  introspectedTable the introspected table<br>
+
      * Inside the function:<br>
-     * 方法在对内省表调用getGeneratedXXXFiles方法之前调用的。插件
-     * 可以实现此方法来覆盖任何默认属性，或更改数据库的结果内省，在任何代码生成活动发生之前。<br>
+     *                          方法在对内省表调用getGeneratedXXXFiles方法之前调用的。<br>
+     *                          插件可以实现此方法来覆盖任何默认属性，或更改数据库的结果<br>
+     *                          内省，在任何代码生成活动发生之前。<br>
+     *Params:<br>
+     * @param introspectedTable the introspected table<br>
+
      */
     @Override
     public void initialized(IntrospectedTable introspectedTable) {
         //首先获取Example文件全路径
         //用参数targetPackage替换包名
         //设置新的Example 文件路径
-        String  oldExampleType=introspectedTable.getExampleType();//获得*Example文件全路径
-        String oldTargetPackage=context.getJavaModelGeneratorConfiguration().getTargetPackage();//获取pojo路径
-        String  exampleType=oldExampleType.replace(oldTargetPackage,targetPackage);
+        String oldExampleType = introspectedTable.getExampleType();//获得*Example文件全路径
+        String oldTargetPackage = context.getJavaModelGeneratorConfiguration().getTargetPackage();//获取pojo路径
+        String exampleType = oldExampleType.replace(oldTargetPackage, targetPackage);
         introspectedTable.setExampleType(exampleType);
 
     }
